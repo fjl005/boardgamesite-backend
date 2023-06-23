66e68be1cf0c2e04e8cb0ad86226d1f361f2537f
Frank Lee <frank.jk.lee96@gmail.com>
2023-06-23 15:40:44 -0700

    updated port listen

--- server.js
+++ server.js
@@ -197,6 +197,8 @@ mongoose.connection.once('open', () => {
     // app.listen(5000, () => console.log('Server Started at Port 5000'));
 });
 
-app.listen(5000, () => {
-    console.log('Server Started at Port 5000');
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
 });
